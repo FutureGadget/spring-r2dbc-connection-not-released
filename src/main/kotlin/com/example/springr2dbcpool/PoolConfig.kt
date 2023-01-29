1870dcefb63ce6ac9462c733d62ac5a3e576a5b3
futuregadget <danwoopark@gmail.com>
2023-01-29 18:10:39 +0900

    change constant

--- src/main/kotlin/com/example/springr2dbcpool/PoolConfig.kt
+++ src/main/kotlin/com/example/springr2dbcpool/PoolConfig.kt
@@ -2,7 +2,7 @@ package com.example.springr2dbcpool
 
 import io.r2dbc.pool.PoolingConnectionFactoryProvider
 import io.r2dbc.spi.ConnectionFactoryOptions
-import io.r2dbc.spi.Option
+import io.r2dbc.spi.ConnectionFactoryOptions.STATEMENT_TIMEOUT
 import org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryOptionsBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,7 +16,7 @@ class PoolConfig {
             builder
                 .option(ConnectionFactoryOptions.CONNECT_TIMEOUT, Duration.ofSeconds(2))
                 .option(
-                    Option.valueOf("statementTimeout"),
+                    STATEMENT_TIMEOUT,
                     Duration.ofSeconds(3)
                 ).option(
                     PoolingConnectionFactoryProvider.MIN_IDLE, 2
